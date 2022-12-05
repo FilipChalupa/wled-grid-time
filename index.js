df2ab50571455b9055dbbec8040f553e70e2677c
Filip Chalupa <chalupa.filip@gmail.com>
2022-12-05 22:47:34 +0100

    Split into columns

--- index.js
+++ index.js
@@ -3,33 +3,20 @@ const red = [255, 0, 0]
 const pink = [230, 0, 126]
 const green = [73, 254, 0]
 const yellow = [255, 255, 0]
+const cyan = [0, 255, 255]
 
 const size = 10
-
-const numbers = [
-	/* 0 */ [1, 1, 1, 0, 1, 1, 1],
-	/* 1 */ [0, 0, 1, 0, 0, 1, 0],
-	/* 2 */ [1, 0, 1, 1, 1, 0, 1],
-	/* 3 */ [1, 0, 1, 1, 0, 1, 1],
-	/* 4 */ [0, 1, 1, 1, 0, 1, 0],
-	/* 5 */ [1, 1, 0, 1, 0, 1, 1],
-	/* 6 */ [1, 1, 0, 1, 1, 1, 1],
-	/* 7 */ [1, 0, 1, 0, 0, 1, 0],
-	/* 8 */ [1, 1, 1, 1, 1, 1, 1],
-	/* 9 */ [1, 1, 1, 1, 0, 1, 1],
-]
 
 const now = new Date()
 const hours = now.getHours()
 const minutes = now.getMinutes()
 const seconds = now.getSeconds()
 
-const timeWidth = 17 - 1 // -1 to make it loop faster
-const xOffset =
-	-timeWidth + (Math.round(now.getTime() / 1000) % (size + timeWidth))
-const yOffset = 2
-
 const rgbColors = new Array(size * size).fill(black)
+
+const changeIntensity = (color, factor) => {
+	return color.map((c) => Math.floor(c * factor))
+}
 
 const paintPixel = (color, x, y) => {
 	if (x >= 0 && y >= 0 && x < size && y < size) {
@@ -37,69 +24,6 @@ const paintPixel = (color, x, y) => {
 		rgbColors[indexTarget] = color
 	}
 }
-
-const paintNumber = (number, x, y) => {
-	const numberMask = numbers[number]
-
-	const allPositions = [
-		/* 0 */ [
-			[x + 0, y + 0],
-			[x + 1, y + 0],
-			[x + 2, y + 0],
-		],
-		/* 1 */ [
-			[x + 0, y + 0],
-			[x + 0, y + 1],
-			[x + 0, y + 2],
-		],
-		/* 2 */ [
-			[x + 2, y + 0],
-			[x + 2, y + 1],
-			[x + 2, y + 2],
-		],
-		/* 3 */ [
-			[x + 0, y + 2],
-			[x + 1, y + 2],
-			[x + 2, y + 2],
-		],
-		/* 4 */ [
-			[x + 0, y + 2],
-			[x + 0, y + 3],
-			[x + 0, y + 4],
-		],
-		/* 5 */ [
-			[x + 2, y + 2],
-			[x + 2, y + 3],
-			[x + 2, y + 4],
-		],
-		/* 6 */ [
-			[x + 0, y + 4],
-			[x + 1, y + 4],
-			[x + 2, y + 4],
-		],
-	]
-	const fillPositions = allPositions.filter((_, index) => numberMask[index] > 0)
-
-	fillPositions.flat().forEach(([x, y]) => {
-		paintPixel(red, x, y)
-	})
-}
-
-const paintDoubleDigit = (number, x, y) => {
-	const firstDigit = Math.floor(number / 10)
-	const secondDigit = number % 10
-	paintNumber(firstDigit, x, y)
-	paintNumber(secondDigit, x + 4, y)
-}
-
-const paintColon = (x, y) => {
-	paintPixel(red, x, y + 1)
-	paintPixel(red, x, y + 3)
-}
-
-// paintDoubleDigit(hours, xOffset, yOffset)
-// paintColon(xOffset + 8, yOffset)
-// paintDoubleDigit(minutes, xOffset + 10, yOffset)
 
 const mirror = (items) => {
 	const newItems = items.map(
@@ -126,28 +50,21 @@ const rotateCounterClockwise = (items) => {
 	return newItems
 }
 
-for (let i = 0; i < Math.floor(seconds / 3); i++) {
-	const y = i % size
-	const x = 6 + Math.floor(i / size)
-	paintPixel(red, x, y)
+const paintColumn = (x, count, color) => {
+	for (let i = 0; i < count; i++) {
+		paintPixel(color, x, i)
+	}
 }
-for (let i = 0; i < (minutes % 3) + 1; i++) {
-	const x = 9
-	const y = i
-	paintPixel(yellow, x, y)
-}
-
-for (let i = 0; i < Math.floor(minutes / 3); i++) {
-	const y = i % size
-	const x = 3 + Math.floor(i / size)
-	paintPixel(green, x, y)
+const paintDoubleDigitColumn = (xStart, number, color) => {
+	const firstDigit = Math.floor(number / 10)
+	const secondDigit = number % 10
+	paintColumn(xStart, firstDigit, color)
+	paintColumn(xStart + 1, secondDigit, changeIntensity(color, 0.5))
 }
 
-for (let i = 0; i < hours; i++) {
-	const y = i % size
-	const x = 0 + Math.floor(i / size)
-	paintPixel(pink, x, y)
-}
+paintDoubleDigitColumn(1, hours, pink)
+paintDoubleDigitColumn(4, minutes, green)
+paintDoubleDigitColumn(7, seconds, red)
 
 const payload = JSON.stringify({
 	seg: {
